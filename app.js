fb1771ab1f874387e6339ac5424b31d96e049836
Sahil Dhanju <sahildhanju1@gmail.com>
2015-10-18 19:59:50 -0500

    start of something I hope

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require ('express');
+
+var app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
+
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now();
+  next();
+};
+
+app.use(requestTime);
+
+app.get('/', function (req, res) {
+  var responseText = 'Hello World!<br>';
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
