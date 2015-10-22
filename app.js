52858867500b7b449867c5c39f8049ea267fe2e6
Sahil Dhanju <sahildhanju1@gmail.com>
2015-10-21 22:04:13 -0500

    Trying to do some more stuff

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-var express = require ('express');
+var express = require('express');
+var request = require('request');
+var fs = require('fs');
+var cheerio = require('cheerio');
 
 var app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 
-var requestTime = function (req, res, next) {
-  req.requestTime = Date.now();
-  next();
-};
+// jsdom.env(
+//   '<p><a class="the-link" href="https://github.com/tmpvar/jsdom">jsdom!</a></p>',
+//   ["http://code.jquery.com/jquery.js"],
+//   function (err, window) {
+//     console.log("contents of a.the-link:", window.$("a.the-link").text());
+//   }
+// );
+var lets;
 
-app.use(requestTime);
-
-app.get('/', function (req, res) {
-  var responseText = 'Hello World!<br>';
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-  res.send(responseText);
+request('https://www.google.com/search?q=green&oq=green&aqs=chrome.0.69i59j0l5.619j0j4&sourceid=chrome&es_sm=93&ie=UTF-8', function (error, response, body) {
+  	$ = cheerio.load(body);
+ //  	fs.writeFile(__dirname + "/outtest.html", $('ol'), function(err) {
+	//     if(err) {
+	//         return console.log(err);
+	//     }
+	//     console.log("The file was saved!");
+	// });
+	body2 = $('ol:not([id]):not([class])');
+	//console.log(body2);
+	$ = cheerio.load(body2.html());
+	//console.log($.html());
+	console.log($('h3.r').text());
 });
+
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
